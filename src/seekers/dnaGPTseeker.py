bfb577b68acdbf385f8596fc30ab4a99c673e14f
StoeckOverflow <dominic-st@gmx.de>
2023-12-06 12:26:53 +0100

    refined dnaGPTseeker

--- src/seekers/dnaGPTseeker.py
+++ src/seekers/dnaGPTseeker.py
@@ -1,18 +1,14 @@
 import math
-from collections import Counter
-import json
 from tqdm import tqdm
 from .seeker import Seeker
 from ..utils.llama_utils import get_probabilities
-#from functools import lru_cache
-# TODO: RunTime Optimization and Decision Function with thresholding, Explanatation Markdown, Integration in seeker class
+# TODO: Remove Stopwords
 class dnaGPTseeker(Seeker):
     
     def __init__(self, disable_tqdm) -> None:
         super().__init__(disable_tqdm)
     
-    #@lru_cache(maxsize=512)
-    def split_input_and_contine(self, text, K=5):
+    def split_input_and_contine(self, text, K=5) -> tuple(str, [str]):
         words = text.split()
         cut_off = len(words) // 2
 
@@ -20,7 +16,7 @@ class dnaGPTseeker(Seeker):
         y0 = ' '.join(words[cut_off:])
         y0_sequences = []
         for i in range(K):
-            generated = self.base_model.create_completion(prompt=x, max_tokens=cut_off)['choices'][0]['text']
+            generated = self.base_model.create_completion(prompt=x, max_tokens=cut_off, temperature=0.7)['choices'][0]['text']
             y0_sequences.append(generated)
 
         #For Debugging purposes:
@@ -30,7 +26,7 @@ class dnaGPTseeker(Seeker):
     
         return y0, y0_sequences
 
-    def ngrams(self,sequence, n):
+    def ngrams(self,sequence, n) -> [str]:
         """Generate n-grams from a sequence."""
         if not isinstance(sequence, (list, str)) or not isinstance(n, int) or n <= 0:
             #print(f"n: {n} \n sequence: {sequence}")
@@ -39,24 +35,29 @@ class dnaGPTseeker(Seeker):
         words = sequence.split()
         return [' '.join(words[i:i + n]) for i in range(len(words) - n + 1)]
 
-    def n_gram_distance(self, y0, y0_sequences, K=5, n0=1, N=9):
+    def n_gram_distance(self, y0, y0_sequences, K=5, n0=1, N=9) -> float:
         'N-Gram Distance Calculation (B-Score)'
         score_sum = 0.0
-        for k in range(1,K):
+        for k in range(K):
             human_text = y0
             model_output = y0_sequences[k-1]
             for n in range(n0, N + 1):
-                human_ngrams = Counter(self.ngrams(human_text, n))
-                model_ngrams = Counter(self.ngrams(model_output, n))
-                intersection = human_ngrams & model_ngrams
-                f_n = n * math.log(n)
-                if len(human_ngrams) * len(model_output) != 0:
-                    term = sum(intersection.values()) / (len(human_ngrams) * len(model_output))
-                    score_sum += term * f_n
-
+                if len(model_output) != 0:
+                    y_0_grams = set(self.ngrams(human_text, n))
+                    if len(y_0_grams) != 0:
+                        y_k_grams = set(self.ngrams(model_output, n))
+                        intersection = set.intersection(y_0_grams, y_k_grams)
+                        print(y_0_grams)
+                        print(y_k_grams)
+                        print(intersection)
+                        print(str(type(intersection)))
+                        print(len(intersection))
+                        f_n = n * math.log(n)
+                        term = len(intersection) / (len(model_output) * len(y_0_grams))
+                        score_sum += term * f_n
         return score_sum / K
 
-    def calculate_evidence(self, y0, y0_sequences, n=3):
+    def calculate_evidence(self, y0, y0_sequences, n=1) -> int:
         """
         Calculate the evidence En as the overlap of n-grams between model output and human text.
         """
@@ -69,7 +70,7 @@ class dnaGPTseeker(Seeker):
 
         return len(En)
 
-    def relative_entropy_distance(self, x, y0_sequences, K):
+    def relative_entropy_distance(self, x, y0_sequences, K) -> float:
         '''
         Relative Entropy Distance Calculation (W-Score)
         ATTENTION: Is not tested now
@@ -94,45 +95,20 @@ class dnaGPTseeker(Seeker):
         return score_sum / (K * min_length)
 
 
-    def calculate_blackbox_scoring_for_newsfeed(self, texts, k=5, distance_method='n-gram'):
+    def calculate_blackbox_scoring_for_newsfeed(self, texts, k=5, distance_method='n-gram') -> [bool]:
         'Do black box scoring for all texts and return bool weather the text has stego or not'
         prediction_results = []
         for text in tqdm(texts, desc=f"Evaluate {distance_method} score for texts", disable=self.disable_tqdm):
             y0, y0_sequences = self.split_input_and_contine(text,k)
             if distance_method == 'n-gram':
-                b_score, evidence = self.n_gram_distance(y0, y0_sequences, k), self.calculate_evidence(y0, y0_sequences)
-                prediction_results.append((b_score, evidence))
+                n_gram_evaluation = self.n_gram_distance(y0, y0_sequences, k) > self.calculate_evidence(y0, y0_sequences)
+                prediction_results.append(n_gram_evaluation)
             elif distance_method == 'entropy':
-                entropy_distance = self.relative_entropy_distance(y0, y0_sequences)
-                prediction_results.append(entropy_distance)
-            
+                entropy_distance_evaluation = self.relative_entropy_distance(y0, y0_sequences) > self.calculate_evidence(y0, y0_sequences)
+                prediction_results.append(entropy_distance_evaluation)
 
-        if distance_method == 'n-gram':
-            print(f"Blackbox results in the format (b_score, evidence): : \n {prediction_results}")
-        elif distance_method == 'entropy':
-            print(f"Entropy results: \n {prediction_results}")
+        print(f"Results: \n {prediction_results}")
         return prediction_results
     
-    def detect_secret(self,newsfeed):
-        prediction_scores = self.calculate_blackbox_scoring_for_newsfeed(newsfeed, distance_method='n-gram')
-        '''
-        For Black Box Method with ngram distance you can also measure the evidence, which serves as an additional score
-        If the evidence is very high, it is very likely the text is machine-written (contains stego)
-        '''
-        decision = decision = any((t[0] > 2 or t[1] > 2) for t in prediction_scores)
-        return decision
-    
-if __name__ == "__main__":
-    #input_data = json.load(sys.stdin)
-    #feed = input_data["feed"]
-    with open('resources/feeds/example_feed.1') as file:
-        input_data_1 = json.load(file)
-    benign_feed = input_data_1['feed']
-    with open('resources/feeds/example_feed.-1') as file:
-        input_data_2 = json.load(file)
-    stego_feed = input_data_2['feed']
-    print('Calculate BlackBox Scoring for benign feed')
-    result = dnaGPTseeker.calculate_blackbox_scoring_for_newsfeed(benign_feed)
-    print('Calculate Blackbox Scoring for Stego feed')
-    result = dnaGPTseeker.alculate_blackbox_scoring_for_newsfeed(stego_feed)
-    #json.dump(result, sys.stdout)
+    def detect_secret(self,newsfeed) -> bool:
+        return True in self.calculate_blackbox_scoring_for_newsfeed(newsfeed)
