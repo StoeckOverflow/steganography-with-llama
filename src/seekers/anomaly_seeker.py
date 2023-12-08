870b06b601dac3ba572bdc8c28f3514580a6178c
Levino614 <levin.ahrens@me.com>
2023-12-08 17:03:07 +0100

    Added entropy and avg_token_prob features

--- src/seekers/anomaly_seeker.py
+++ src/seekers/anomaly_seeker.py
@@ -1,21 +1,16 @@
 import zipfile
-import os
+from tqdm import tqdm
 import json
 import numpy as np
 from llama_cpp import Llama
-import nltk
-import spacy
-from spellchecker import SpellChecker
 from collections import Counter
 from itertools import groupby
 from sklearn.preprocessing import StandardScaler
-from textstat import flesch_reading_ease
-nltk.download('averaged_perceptron_tagger', quiet=True)
-nltk.download('punkt', quiet=True)
-from sklearn.svm import OneClassSVM
-from sklearn.ensemble import IsolationForest
+from sklearn.ensemble import RandomForestClassifier
 import joblib
-from transformers import AutoTokenizer
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 
 def load_data(archive_path):
     articles = []
@@ -35,142 +30,80 @@ def clean(text):
     text = text.replace("-", " ")
     return text
 
-def get_average_token_probability(llm, tokens):
-    token_probabilities = []
-    for token in tokens:
-        # Skip the first 5 tokens 
-        if tokens.index(token) < 5:
-            continue
-        # Take previous tokens as prompt to get probability of next token
-        prompt = ' '.join(tokens[:tokens.index(token)])
-        nr_tokens_to_generate = 1000
-        next_token_logprobs = llm(prompt, top_p=0, max_tokens=1, logprobs=nr_tokens_to_generate, temperature= 0, top_k=1)['choices'][0]['logprobs']['top_logprobs'][0]
-        # Get probability of next token
-        try:
-            token_prob = np.exp(float(next_token_logprobs[token]))
-        except KeyError:
-            token_prob = 0.0
-        print(f"Prompt: {prompt}, Token: {token}, Probability: {token_prob}")
-        # Add probability to list
-        token_probabilities.append(token_prob)
-        print()
-    return np.mean(token_probabilities)
+def create_dataset(dir_path_benign, dir_path_malicious):
+    benign_articles = load_data(dir_path_benign)
+    benign_labels = np.ones(len(benign_articles))
+    malicious_articles = load_data(dir_path_malicious)
+    malicious_labels = np.ones(len(malicious_articles)) * -1
 
-def grammar_check(tokens):
-    pos_tags = nltk.pos_tag(tokens)
+    df_benign = pd.DataFrame({'article': benign_articles, 'label': benign_labels})
+    df_malicious = pd.DataFrame({'article': malicious_articles, 'label': malicious_labels})
+    df = pd.concat([df_benign, df_malicious])
+    return df
 
-    def check_subject_verb_agreement(pos_tags):
-        for i in range(len(pos_tags) - 1):
-            if pos_tags[i][1] in ['NN', 'NNS'] and pos_tags[i+1][1] in ['VBZ', 'VBP']:
-                if pos_tags[i][1] == 'NN' and pos_tags[i+1][1] != 'VBZ':
-                    return False
-                if pos_tags[i][1] == 'NNS' and pos_tags[i+1][1] != 'VBP':
-                    return False
-        return True
+def shannon_entropy(article):
+    prob = [ float(article.count(c)) / len(article) for c in dict.fromkeys(list(article)) ]
+    entropy = - sum([ p * np.log2(p) for p in prob ])
+    return entropy
 
-    # TODO: Implement more grammar checks
-    # Function to check tense consistency
-    def check_tense_consistency(pos_tags):
-        # Implement specific checks here
-        pass
-    # Function to check plural/singular agreement
-    def check_plural_singular_agreement(pos_tags):
-        # Implement specific checks here
-        pass
-    # Function to check pronoun-antecedent agreement
-    def check_pronoun_antecedent_agreement(pos_tags):
-        # Implement specific checks here
-        pass
-    # Function to check pronoun case
-    def check_pronoun_case(pos_tags):
-        # Implement specific checks here
-        pass
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
-    sub_verb_agreement = check_subject_verb_agreement(pos_tags)
-    tense_consistency = check_tense_consistency(pos_tags)
-    plural_singular_agreement = check_plural_singular_agreement(pos_tags)
-    pronoun_antecedent_agreement = check_pronoun_antecedent_agreement(pos_tags)
-    pronoun_case = check_pronoun_case(pos_tags)
-
-    return sub_verb_agreement, tense_consistency, plural_singular_agreement, pronoun_antecedent_agreement, pronoun_case
+def special_chars_count(article):
+    special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '?']
+    return sum([article.count(char) for char in special_chars])
 
 def extract_features(articles):
     llm = Llama(model_path='resources/llama-2-7b.Q5_K_M.gguf', logits_all=True, verbose=False)
     features = []
-    for article in articles:
+    for article in tqdm(articles, desc='Extracting features'):
         article = str(article)
-        #tokens = nltk.word_tokenize(article) # TODO: use LLaMA tokenizer instead
-        tokenizer = AutoTokenizer.from_pretrained("stevhliu/my_awesome_model")
-        tokens = tokenizer.tokenize(article)
-        print(tokens)
+        tokens = llm.tokenizer().encode(article)
 
         # Article length
         length = len(article)
 
-        # Average sentence length
-        #avg_sentence_length = np.mean([len(sentence) for sentence in nltk.sent_tokenize(article)])
-
         # Vocabulary richness
-        #vocab_richness = len(set(tokens)) / length if length > 0 else 0
-
-        # Readability score
-        #readability = flesch_reading_ease(article)
-        #print(f'Readability score: {readability}')  # --> found this to be irrelevant
-
-        # Named Entities Count
-        #nlp = spacy.load("en_core_web_sm")
-        #doc = nlp(article)
-        #num_entities = len(doc.ents)
-        #print(f'Named Entities Count: {num_entities}')
-        #print("\tText:", [(ent.text) for ent in doc.ents])
-        #print("\tEntities:", [(ent.text, ent.label_) for ent in doc.ents])  # --> found this to be irrelevant
-
-        # Grammar Check 
-        sub_verb_agreement, tense_consistency, plural_singular_agreement, pronoun_antecedent_agreement, pronoun_case = grammar_check(tokens)
-
-        # Spell Check (count of misspelled words)
-        #spell = SpellChecker()
-        #spell.word_frequency.load_words([]) # Add custom words here
-        #misspelled = spell.unknown(tokens)
-        #num_misspelled = len(misspelled)
-        #print(f'Misspelled words: {num_misspelled} ({misspelled})')  # --> found this to be irrelevant (too many false positives)
+        vocab_richness = len(set(tokens)) / length if length > 0 else 0
 
         # Special characters count
-        special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', "'", '<', '>', ',', '.', '?']
-        num_special_chars = sum([article.count(char) for char in special_chars])
+        num_special_chars = special_chars_count(article)
 
-        # Max consecutive special characters
-        max_consecutive_special_chars = max([len(list(g)) for k, g in groupby(article) if k in special_chars])
+        # Shannon entropy
+        entropy = shannon_entropy(article)
 
-        # Lower case count after sentence end
-        #lower_case_count = sum([1 for i in range(len(tokens)) if tokens[i-1] == '.' and tokens[i].islower()])
-
-        # Average next token probability
-        avg_token_prob = 0.0
-        # Try to load from file, if not possible, calculate and save to file
-        #try: 
-        #    with open('resources/models/avg_token_prob.json', 'r') as f:
-        #        avg_token_prob = json.load(f)['avg_token_prob']
-        #except FileNotFoundError:
-        #    avg_token_prob = get_average_token_probability(llm, tokens)  # --> very slow
-        #    with open('resources/models/avg_token_prob.json', 'w') as f:
-        #        json.dump({'avg_token_prob': avg_token_prob}, f)
+        # Average token probability
+        llm.reset()
+        llm.eval(tokens)
+        logits = np.array(llm._scores)
+        token_probabilities = softmax(logits)
+        avg_token_probability = np.mean(token_probabilities)
 
         # Collecting all features
-        article_features = [length, num_special_chars, max_consecutive_special_chars, avg_token_prob]
+        article_features = [length, vocab_richness, num_special_chars, entropy, avg_token_probability]
         features.append(article_features)
 
     scaler = StandardScaler()
     transformed_features = scaler.fit_transform(features)
-    return transformed_features
+    df = pd.DataFrame(transformed_features, columns=['length', 'vocab_richness', 'num_special_chars', 'entropy', 'avg_token_probability'])
+    return df
 
-def train_model(path):
-    articles = load_data(path)
+def train_model(benign_data_path, malicious_data_path):
+    dataset = create_dataset(benign_data_path, malicious_data_path)
+    articles = dataset['article']
+    labels = dataset['label']
     features = extract_features(articles)
 
-    #clf = OneClassSVM(gamma='auto', kernel='rbf')
-    clf = IsolationForest(random_state=420)
-    clf = clf.fit(features)
+    # Train test split
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=420, stratify=labels)
+
+    clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+    clf = clf.fit(X_train, y_train)
+
+    f1 = f1_score(y_test, clf.predict(X_test))
+    print(f"F1 Score: {f1}")
+
     joblib.dump(clf, 'resources/models/anomaly_detector.joblib')
 
 def predict_single_feed(path):
@@ -182,23 +115,28 @@ def predict_single_feed(path):
     clf = joblib.load('resources/models/anomaly_detector.joblib')
     predictions = clf.predict(features)
     counts = Counter(predictions)
-    print(f"Counts: {counts}")
-    if counts[1]/(counts[1]+counts[-1]) > 0.85:  # Set threshold for number of anomalies in newsfeed
+    print('Predictions:', predictions)
+    if counts[1]/(counts[1]+counts[-1]) > 0.6:  # Set threshold for number of anomalies in newsfeed
         return 1
     else:
         return -1
 
 def main():
-    training_data_path = 'resources/feeds/clean_feeds.zip'
-    train_model(training_data_path)
+    benign_data_path = 'resources/feeds/clean_feeds.zip'
+    malicious_data_path = 'resources/doctored_feeds/doctored_feeds.zip'
+    train_model(benign_data_path, malicious_data_path)
 
-    #benign_example = 'resources/feeds/example_feed.-1'
-    #result = predict_single_feed(benign_example)
-    #print(f"Prediction for benign example: {result}")
+    benign_example = 'resources/feeds/feed_003.json'
+    result = predict_single_feed(benign_example)
+    print(f"Prediction for benign example: {result}")
+    print('True label: 1')
 
-    #malicious_example = 'resources/feeds/example_feed.1'
-    #result = predict_single_feed(malicious_example)
-    #print(f"Prediction for malicious example: {result}")
+    print()
+
+    malicious_example = 'resources/doctored_feeds/doctored_feed_003.json'
+    result = predict_single_feed(malicious_example)
+    print(f"Prediction for malicious example: {result}")
+    print('True label: -1')
 
 if __name__ == '__main__':
     main()
