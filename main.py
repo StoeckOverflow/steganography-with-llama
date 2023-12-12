84c4fb47e628654fa08d0224f813a6b7bec86071
StoeckOverflow <dominic-st@gmx.de>
2023-12-12 09:27:35 +0100

    last additions

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #from src.models import DynamicPOE
-#from src.seekers.anomaly_seeker.anomaly_seeker import Anomaly_Seeker
-from src.seekers.detectGPTseeker import detectGPTseeker
+#from src.seekers.detectGPTseeker import detectGPTseeker
+from src.seekers.anomaly_seeker.anomaly_seeker import Anomaly_Seeker
 
 if __name__ == "__main__":
     #dpoe = DynamicPOE(disable_tqdm=True)
     #dpoe.recover_interface()
-    #seeker = Anomaly_Seeker(disable_tqdm=False)
+    #seeker = detectGPTseeker(disable_tqdm=False)
     #seeker.detection_interface()
-    seeker = detectGPTseeker(disable_tqdm=False)
+    seeker = Anomaly_Seeker(disable_tqdm=True)
     seeker.detection_interface()
