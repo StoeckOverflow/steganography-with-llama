6425ef10efc28bfc91b9cb3e577b584928eb3316
StoeckOverflow <dominic-st@gmx.de>
2024-01-04 21:53:07 +0100

    refined download script

--- load_bert_model_locally.sh
+++ load_bert_model_locally.sh
@@ -3,4 +3,3 @@ wget https://huggingface.co/bert-base-cased/resolve/main/config.json -P resource
 wget https://huggingface.co/bert-base-cased/resolve/main/pytorch_model.bin -P resources/bert-base-cased
 wget https://huggingface.co/bert-base-cased/resolve/main/tokenizer_config.json -P resources/bert-base-cased
 wget https://huggingface.co/bert-base-cased/resolve/main/tokenizer.json -P resources/bert-base-cased
-wget https://huggingface.co/bert-base-cased/resolve/main/vocab.txt -P resources/bert-base-cased
