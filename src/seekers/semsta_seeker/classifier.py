f84439d1a6b5d98161b8255b9d946b7676162573
StoeckOverflow <dominic-st@gmx.de>
2024-02-03 14:21:16 +0100

    renewed the architecture

--- src/seekers/semsta_seeker/classifier.py
+++ src/seekers/semsta_seeker/classifier.py
@@ -3,84 +3,149 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 '''DEPRECATED
-class Classifier(nn.Module):
-    def __init__(self, input_dim: int, num_classes: int):
-        super(Classifier, self).__init__()
-        self.attention = nn.Sequential(
-            nn.Linear(input_dim, input_dim),
-            nn.Tanh(),
-            nn.Linear(input_dim, 1)
-        )
-        self.classifier = nn.Linear(input_dim, num_classes)
-
-    def forward(self, fused_features):
-        attention_weights = F.softmax(self.attention(fused_features), dim=1)
-        attention_applied = fused_features * attention_weights.expand_as(fused_features)
-        
-        aggregated_features = attention_applied.sum(1)
-        
-        logits = self.classifier(aggregated_features)
-        return logits
-'''
-
 class BahdanauAttention(nn.Module):
     def __init__(self, key_size, query_size, hidden_size):
         super(BahdanauAttention, self).__init__()
         self.key_layer = nn.Linear(key_size, hidden_size)
         self.query_layer = nn.Linear(query_size, hidden_size)
         self.energy_layer = nn.Linear(hidden_size, 1)
+        #self.dropout = nn.Dropout(0.1)
 
     def forward(self, query, keys):
-        # query = [batch_size, query_size]
-        # keys = [batch_size, seq_len, key_size]
+        transformed_query = self.query_layer(query)  # [batch_size, seq_len, hidden_size]
+        transformed_keys = self.key_layer(keys)  # [batch_size, seq_len, hidden_size]
+        
+        # Compute energy scores
+        energy = torch.tanh(transformed_query + transformed_keys)  # Broadcasting is automatically handled
+        #energy = self.dropout(energy)
+        energy = self.energy_layer(energy).squeeze(-1)  # [batch_size, seq_len]
 
-        query = query.unsqueeze(1).repeat(1, keys.size(1), 1)  # [batch_size, seq_len, query_size]
-        keys = self.key_layer(keys)  # [batch_size, seq_len, hidden_size]
-
-        energy = torch.tanh(keys + query)  # [batch_size, seq_len, hidden_size]
-        energy = self.energy_layer(energy).squeeze(2)  # [batch_size, seq_len]
-
+        # Compute attention weights
         attention = F.softmax(energy, dim=1)  # [batch_size, seq_len]
         return attention
-
 class Classifier(nn.Module):
-    def __init__(self, hidden_size, dense_units, num_classes=2):
+    def __init__(self, statistical_dim, semantic_dim, num_classes=2):
         super(Classifier, self).__init__()
-        
-        self.attention = BahdanauAttention(hidden_size, hidden_size, hidden_size)
-        self.dense_layer = nn.Linear(hidden_size, dense_units)
-        self.output_layer = nn.Linear(dense_units, num_classes)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.5)
+        self.attention = BahdanauAttention(statistical_dim, semantic_dim)
+        self.dense_layer = nn.Linear(statistical_dim, semantic_dim)
+        self.classification_layer = nn.Linear(semantic_dim, num_classes)
 
     def forward(self, fused_features, original_features):
+        print('begin')
+        # Attention mechanism
         attention_weights = self.attention(fused_features, original_features)
-        context_vector = torch.bmm(attention_weights.unsqueeze(1), original_features).squeeze(1)
+        attention_weights = attention_weights.unsqueeze(1)
+        fused_vector = torch.bmm(attention_weights, original_features).squeeze(1)
+        
+        #aggregated_vector = torch.mean(fused_vector, dim=1)
+        #print('Aggregated Vector Shape')
+        #print(aggregated_vector.shape)
+        aggregate_layer =  self.dense_layer(fused_vector)
+        print('Aggregated Layer Shape after dense layer')
+        print(aggregate_layer.shape)
+        
+        output_vector = self.classification_layer(aggregate_layer)
+        print('Output vector shape')
+        print(output_vector.shape)
+        probability_vector = torch.softmax(output_vector, dim=1)
+        print('Probability Vector shape')
+        print(probability_vector.shape)
+        print(probability_vector)
 
-        dense_output = self.relu(self.dense_layer(context_vector))
-        dense_output = self.dropout(dense_output)
-        output = self.output_layer(dense_output)
+        print('end')
+        return probability_vector
 
-        return output
+''' 
 
-    def train_classifier(self, original_encoder_features, fused_features, train_labels, num_epochs=100, learning_rate=0.001, batch_size=128):
-        self.train()
+class BahdanauAttention(nn.Module):
+    def __init__(self, n_hidden_enc, n_hidden_dec):
+        
+        super(BahdanauAttention, self).__init__()
+        
+        self.h_hidden_enc = n_hidden_enc
+        self.h_hidden_dec = n_hidden_dec
+        
+        self.W = nn.Linear(n_hidden_enc + n_hidden_dec, n_hidden_dec, bias=False) 
+        self.V = nn.Parameter(torch.rand(n_hidden_dec))
+
+    def forward(self, hidden_dec, last_layer_enc):
+        ''' 
+            PARAMS:           
+                hidden_dec:     [b, n_layers, n_hidden_dec]    (1st hidden_dec = encoder's last_h's last layer)                 
+                last_layer_enc: [b, seq_len, n_hidden_enc * 2] 
+            
+            RETURN:
+                att_weights:    [b, src_seq_len] 
+        '''
+        batch_size = last_layer_enc.size(0)
+        src_seq_len = last_layer_enc.size(1)
+
+        hidden_dec = hidden_dec[:, -1, :].unsqueeze(1).repeat(1, src_seq_len, 1)         #[b, src_seq_len, n_hidden_dec]
+
+        tanh_W_s_h = torch.tanh(self.W(torch.cat((hidden_dec, last_layer_enc), dim=2)))  #[b, src_seq_len, n_hidden_dec]
+        tanh_W_s_h = tanh_W_s_h.permute(0, 2, 1)       #[b, n_hidde_dec, seq_len]
+        
+        V = self.V.repeat(batch_size, 1).unsqueeze(1)  #[b, 1, n_hidden_dec]
+        e = torch.bmm(V, tanh_W_s_h).squeeze(1)        #[b, seq_len]
+        
+        att_weights = F.softmax(e, dim=1)              #[b, src_seq_len]
+        
+        return att_weights
+
+class Classifier(nn.Module):
+    def __init__(self, statistical_dim, semantic_dim, num_classes=2):
+        super(Classifier, self).__init__()
+        self.attention = BahdanauAttention(statistical_dim, semantic_dim)
+        self.dense_layer = nn.Linear(statistical_dim, semantic_dim)
+        self.classification_layer = nn.Linear(semantic_dim, num_classes)
+
+    def forward(self, fused_features, original_features):
+        #print('begin')
+        # Attention mechanism
+        attention_weights = self.attention(fused_features, original_features)
+        attention_weights = attention_weights.unsqueeze(1)
+        fused_vector = torch.bmm(attention_weights, original_features).squeeze(1)
+        
+        aggregate_layer =  self.dense_layer(fused_vector)
+        output_vector = self.classification_layer(aggregate_layer)
+
+        #probability_vector = torch.softmax(output_vector, dim=1)
+        #train_predicted_labels = torch.argmax(probability_vector, dim=1)
+        #print('end')
+        return output_vector
+
+class Classifier_Trainer():
+    def __init__(self, classifier: Classifier):
+        self.classifier = classifier
+
+    def init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+    
+    def train_classifier(self, original_encoder_features, fused_features, train_labels, num_epochs=100, learning_rate=0.0001, batch_size=64):
+        self.classifier.train()
         numeric_train_labels = [(int(float(label)) + 1) // 2 for label in train_labels]
         train_labels = torch.tensor(numeric_train_labels, dtype=torch.long)
 
         train_dataset = TensorDataset(original_encoder_features, fused_features, train_labels)
         train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-        optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+        optimizer = torch.optim.Adam(self.classifier.parameters(), lr=learning_rate)
         criterion = nn.CrossEntropyLoss()
 
         for epoch in range(num_epochs):
             total_loss = 0.0
             for batch_original_encoder_features, batch_fused_features, batch_labels in train_loader:
-
-                outputs = self(batch_fused_features, batch_original_encoder_features)
+                
+                outputs = self.classifier(batch_fused_features, batch_original_encoder_features)
+                outputs = torch.squeeze(outputs, dim=1)
                 loss = criterion(outputs, batch_labels)
 
                 optimizer.zero_grad()
@@ -92,11 +157,109 @@ class Classifier(nn.Module):
             print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss/len(train_loader)}')
             
         torch.save(self.state_dict(), 'resources/models/classifier.pth')
+    
+    def train_classifier_with_cross_validation(self, original_encoder_features, fused_features, train_labels, num_epochs=100, learning_rate=0.0001, batch_size=64, k_folds=5):        
+        numeric_train_labels = [1 if int(label) == -1 else 0 for label in train_labels]
+        train_labels = torch.tensor(numeric_train_labels, dtype=torch.long)
+        y_numpy = train_labels.numpy()
         
-    def evaluate_classifier(self, original_encoder_features, fused_features, test_labels, batch_size=128):
-        self.eval()
+        skf = StratifiedKFold(n_splits=k_folds, shuffle=True, random_state=42)
         
-        numeric_test_labels = [(int(float(label)) + 1) // 2 for label in test_labels]
+        for fold, (train_idx, val_idx) in enumerate(skf.split(np.zeros(len(y_numpy)), y_numpy)):
+            print(f"Training on fold {fold+1}/{k_folds}...")
+            
+            # Re-initialize model here if necessary to reset weights for each fold
+            self.classifier.apply(self.init_weights)
+            
+            train_idx_tensor = torch.tensor(train_idx, dtype=torch.long)
+            val_idx_tensor = torch.tensor(val_idx, dtype=torch.long)
+
+            # Prepare training and validation datasets
+            train_dataset = TensorDataset(original_encoder_features[train_idx_tensor], fused_features[train_idx_tensor], train_labels[train_idx_tensor])
+            val_dataset = TensorDataset(original_encoder_features[val_idx_tensor], fused_features[val_idx_tensor], train_labels[val_idx_tensor])
+
+            train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+            val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
+
+            '''
+            no_decay = ['bias', 'LayerNorm.weight']
+            optimizer_grouped_parameters = [
+                {'params': [p for n, p in self.classifier.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+                {'params': [p for n, p in self.classifier.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            ]
+            '''
+            
+            optimizer = torch.optim.Adam(self.classifier.parameters() , lr=learning_rate)
+            criterion = nn.CrossEntropyLoss()
+            
+            #scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
+
+            for epoch in range(num_epochs):
+                self.classifier.train()  # Set model to training mode
+                total_loss = 0.0
+                all_train_predictions = []
+                all_train_targets = []
+
+                # Training loop
+                for batch_original_encoder_features, batch_fused_features, batch_labels in train_loader:
+                    outputs = self.classifier(batch_fused_features, batch_original_encoder_features)
+                    #print(outputs)
+                    #print(outputs.shape)
+                    #print(batch_labels)
+                    #print(batch_labels.shape)
+                    loss = criterion(outputs, batch_labels)
+                    
+                    probability_vector = torch.softmax(outputs, dim=1)
+                    train_predicted_labels = torch.argmax(probability_vector, dim=1)
+                    all_train_predictions.extend(train_predicted_labels.detach().numpy())
+                    all_train_targets.extend(batch_labels.cpu().numpy())
+
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                    total_loss += loss.item()
+                    
+                f1_train = f1_score(all_train_targets, all_train_predictions, average='binary')
+                acc_train = accuracy_score(all_train_targets, all_train_predictions)
+                rec_train = recall_score(all_train_targets, all_train_predictions, average='binary')
+                pre_train = precision_score(all_train_targets, all_train_predictions, average='binary', zero_division=0)
+
+                #print(f'Fold {fold+1}, Epoch [{epoch+1}/{num_epochs}], Training Loss: {total_loss/len(train_loader)}')
+                print(f'Fold {fold+1}, Epoch [{epoch+1}/{num_epochs}], Training Loss: {total_loss/len(train_loader)}, F1 Score: {f1_train:.4f}, Precision: {pre_train:.4f}, Recall: {rec_train:.4f}, Accuracy: {acc_train:.4f}')
+
+                # Validation loop
+                self.classifier.eval()  # Set model to evaluation mode
+                val_loss = 0.0
+                all_predictions = []
+                all_targets = []
+
+                with torch.no_grad():
+                    for batch_original_encoder_features, batch_fused_features, batch_labels in val_loader:
+                        outputs = self.classifier(batch_fused_features, batch_original_encoder_features)                        
+                        
+                        #outputs = torch.squeeze(outputs, dim=1)
+                        loss = criterion(outputs, batch_labels)
+                        val_loss += loss.item()
+
+                        probabilities_vector = torch.softmax(outputs, dim=1)
+                        predicted_labels = torch.argmax(probabilities_vector, dim=1)
+                        all_predictions.extend(predicted_labels.cpu().numpy())
+                        all_targets.extend(batch_labels.cpu().numpy())
+                
+                #avg_val_loss = val_loss / len(val_loader)
+                #scheduler.step(avg_val_loss)
+
+                f1 = f1_score(all_targets, all_predictions, average='binary')
+                acc = accuracy_score(all_targets, all_predictions)
+                rec = recall_score(all_targets, all_predictions, average='binary')
+                pre = precision_score(all_targets, all_predictions, average='binary', zero_division=0)
+                print(f'Fold {fold+1}, Epoch [{epoch+1}/{num_epochs}], Validation Loss: {val_loss/len(val_loader)}, F1 Score: {f1:.4f}, Precision: {pre:.4f}, Recall: {rec:.4f}, Accuracy: {acc:.4f}')
+
+    def evaluate_classifier(self, original_encoder_features, fused_features, test_labels, batch_size=64):
+        self.classifier.eval()
+        
+        numeric_test_labels = [1 if int(label) == -1 else 0 for label in test_labels]
         test_labels = torch.tensor(numeric_test_labels, dtype=torch.long)
         
         test_dataset = TensorDataset(original_encoder_features, fused_features, test_labels)
@@ -107,18 +270,17 @@ class Classifier(nn.Module):
         
         with torch.no_grad():
             for batch_original_encoder_features, batch_fused_features, batch_labels in test_loader:
-                
-                outputs = self(batch_fused_features, batch_original_encoder_features)
-                probabilities = torch.softmax(outputs, dim=1)[:, 1]
-                predicted_labels = torch.argmax(probabilities, dim=1).tolist()
-
+                outputs = self.classifier(batch_fused_features, batch_original_encoder_features)
+                outputs = torch.squeeze(outputs, dim=1)
+                probabilities = torch.softmax(outputs, dim=1)
+                predicted_labels = torch.argmax(probabilities, dim=1)
                 all_predictions.extend(predicted_labels.cpu().numpy())
                 all_targets.extend(batch_labels.cpu().numpy())
 
         accuracy = accuracy_score(all_targets, all_predictions)
-        precision = precision_score(all_targets, all_predictions)
-        recall = recall_score(all_targets, all_predictions)
-        f1 = f1_score(all_targets, all_predictions)
+        precision = precision_score(all_targets, all_predictions, average='binary', zero_division=0)
+        recall = recall_score(all_targets, all_predictions, average='binary')
+        f1 = f1_score(all_targets, all_predictions, average='binary')
         
         print(f"Test Accuracy: {accuracy:.4f}")
         print(f"Precision: {precision:.4f}")
--- src/seekers/semsta_seeker/semanticFeatureExtractor.py
+++ src/seekers/semsta_seeker/semanticFeatureExtractor.py
@@ -2,11 +2,12 @@ from transformers import BertTokenizer, BertModel
 import torch.nn as nn
 import torch
 
+'''DEPRECATED
 class SemanticFeatureExtractor(nn.Module):
-    def __init__(self, output_size, bert_model_name='bert-base-cased'):
+    def __init__(self, output_size, bert_model_name='bert-base-uncased'):
         super(SemanticFeatureExtractor, self).__init__()
         self.bert_model = BertModel.from_pretrained(bert_model_name)
-        self.bert_tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+        self.bert_tokenizer = BertTokenizer.from_pretrained(bert_model_name)
         self.dense = nn.Linear(self.bert_model.config.hidden_size, output_size)
         self.sigmoid = nn.Sigmoid()
 
@@ -20,3 +21,41 @@ class SemanticFeatureExtractor(nn.Module):
         confidence_scores = self.sigmoid(dense_output)
 
         return confidence_scores
+'''
+  
+class SemanticFeatureExtractor(nn.Module):
+    def __init__(self, bert_model_name='bert-base-uncased', output_size=1024):
+        super(SemanticFeatureExtractor, self).__init__()
+        self.bert_model = BertModel.from_pretrained(bert_model_name)
+        self.bert_tokenizer = BertTokenizer.from_pretrained(bert_model_name)
+        self.dense = nn.Linear(self.bert_model.config.hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, newsfeed):
+        inputs = self.bert_tokenizer(newsfeed, padding=True, truncation=True, return_tensors="pt")
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
+        outputs = self.bert_model(input_ids=input_ids, attention_mask=attention_mask)
+        last_layer_output = outputs.last_hidden_state  # Shape: (batch_size, seq_length, hidden_size)
+
+        # Apply mean pooling across the sequence length dimension
+        mask_expanded = attention_mask.unsqueeze(-1).expand(last_layer_output.size()).float()
+        sum_embeddings = torch.sum(last_layer_output * mask_expanded, 1)
+        sum_mask = mask_expanded.sum(1)  # This avoids division by zero
+        mean_pooled_output = sum_embeddings / sum_mask
+
+        dense_output = self.dense(mean_pooled_output)  # Now shape: (batch_size, output_size)
+        confidence_scores = self.sigmoid(dense_output)
+        return confidence_scores
+    '''
+    def forward(self, newsfeed):
+        inputs = self.bert_tokenizer(newsfeed, padding=True, truncation=False, return_tensors="pt")
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
+        outputs = self.bert_model(input_ids=input_ids, attention_mask=attention_mask)
+        last_layer_output = outputs.last_hidden_state  # Shape: (batch_size, seq_length, hidden_size)
+        dense_output = self.dense(last_layer_output)
+        confidence_scores = self.sigmoid(dense_output)
+        averaged_confidence_scores = confidence_scores.mean(dim=1)
+        return confidence_scores
+    '''
